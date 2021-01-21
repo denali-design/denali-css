379d9dcac01e7c896ba92a08abc5b839d1c8560a
Anusha Ganti <annapoorna.ganti@verizonmedia.com>
2021-01-21 15:44:03 -0500

    Lists and Loaders CSS documentation

---
+++ stories/lists.stories.js
@@ -0,0 +1,19 @@
+export default {
+  title: "Components/Lists",
+};
+
+export const OrderedList = () => {
+  return `<ul class="list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>`;
+};
+
+export const UnorderedList = () => {
+  return `<ol class="list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ol>`;
+};
