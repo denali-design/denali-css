53a2ee9cbef1fa096ca41fbfbadbbd059125ffe7
Anusha Ganti <annapoorna.ganti@verizonmedia.com>
2021-06-25 10:44:45 -0400

    Accordion initial commit

---
+++ stories/accordion.stories.js
@@ -0,0 +1,64 @@
+// import './accordion.css';
+
+export default {
+    title: 'Components/Accordion',
+  };
+  
+
+
+  export const Default = ({isOpen, hasIcon}) => {
+    
+    return (
+      `<div class="accordion has-icon">
+      <details>
+       <summary>
+        Title</summary>
+        <div class="content">
+       Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.
+       </div>
+      </details>
+      </div>`
+    );
+  };
+  
+
+
+  export const Playground = ({isOpen, hasIcon}) => {
+    
+    return (
+      `<div class="accordion ${hasIcon ? 'has-icon' : ''}">
+      <details ${isOpen ? 'open' : ''}  >
+       <summary>
+        Title</summary>
+        <div class="content">
+       Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.
+       </div>
+      </details>
+      <details  >
+       <summary>
+        Title</summary>
+        <div class="content">
+       Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.
+       </div>
+      </details>
+      <details  >
+       <summary>
+        Title</summary>
+        <div class="content">
+       Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.
+       </div>
+      </details>
+      </div>`
+    );
+  };
+  
+  Playground.argTypes = {
+    isOpen: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    hasIcon: {
+      defaultValue : true,
+      control: 'boolean',
+    }
+  };
