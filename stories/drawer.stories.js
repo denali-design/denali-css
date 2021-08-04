4d88bdfff2f09c125245bc004bc6d37f7355d58a
Anusha Ganti <annapoorna.ganti@verizonmedia.com>
2021-08-04 13:03:58 -0400

    Drawer Component

--- stories/drawer.stories.js
+++ stories/drawer.stories.js
@@ -1,34 +1,58 @@
 export default {
-    title: 'Components/Drawer',
-  };
-  
-  export const Default = ({size, hasHeader, hasFooter, hasCloseIcon}) => {
-    return (
-      `<div class="drawer" style="width: ${size}px">
-        ${hasHeader? `<div class="drawer-header">Title</div>`: ''}
+  title: "Components/Drawer",
+};
+
+export const Default = () => {
+  return `<div class="drawer is-left" >
         <div class="drawer-content">Content
-        ${hasCloseIcon? `<a href="#close" class="close"><i class="d-icon d-close"></i></a>` : ''}
         </div>
-        ${hasFooter? `<div class="drawer-footer">Footer</div>`: ''}
-      </div>`
-    );
-  };
-  
-  Default.argTypes = {
-    size: {
-      defaultValue: 340,
-      control: 'number',
+      </div>`;
+};
+
+export const Playground = ({
+  isInverse,
+  position,
+  hasHeader,
+  hasFooter,
+  hasCloseIcon,
+}) => {
+  return `<div class="drawer ${isInverse ? "is-inverse" : ""} ${
+    position ? `is-${position}` : ""
+  }" >
+        ${hasHeader ? `<div class="drawer-header">Title</div>` : ""}
+        <div class="drawer-content">Content
+        ${
+          hasCloseIcon
+            ? `<a href="#close" class="close"><i class="d-icon d-close"></i></a>`
+            : ""
+        }
+        </div>
+        ${hasFooter ? `<div class="drawer-footer">Footer</div>` : ""}
+      </div>`;
+};
+
+Playground.argTypes = {
+  isInverse: {
+    defaultValue: false,
+    control: "boolean",
+  },
+  position: {
+    defaultValue: "left",
+    control: {
+      type: "select",
+      options: ["left", "right", "top", "bottom"],
     },
-    hasHeader:{
-        defaultValue: false,
-        control: 'boolean',
-    },
-    hasFooter:{
-        defaultValue: false,
-        control: 'boolean',
-    },
-    hasCloseIcon:{
-      defaultValue: false,
-      control: 'boolean',
-    }
-  };
+  },
+  hasHeader: {
+    defaultValue: false,
+    control: "boolean",
+  },
+  hasFooter: {
+    defaultValue: false,
+    control: "boolean",
+  },
+  hasCloseIcon: {
+    defaultValue: false,
+    control: "boolean",
+  },
+};
