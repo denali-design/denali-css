f0ba56cc91179cadf50dcc6af71588c6c7605406
Trevor Greenleaf <trevor.greenleaf@yahooinc.com>
2023-04-16 16:41:06 -0700

    Added Daisy UI to Build
    - Removed old V2 packages
    - Removed change Log
    - Added node_modules, and *.log files to gitignore
    - New package json file
    - New Index.js setup file for v3
    - Temp remove Storybook files
    - Temp removed screwdriver yaml setup file
    - Temp removed test suite

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import DenaliTheme from './DenaliTheme';
-
-addons.setConfig({
-  theme: DenaliTheme,
-});
--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-/**
- * Copyright 2023, Yahoo Inc.
- * Licensed under the terms of the MIT license. See LICENSE file in project root for terms
- */
+import daisyui from 'daisyui';
+import { install } from 'daisyui';
 
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-
-module.exports = {
-    getFilePath(name) {
-        return path.resolve(__dirname, name || '');
-    }
+const DenaliCustomTheme = {
+  themes: [
+    {
+      name: 'my-theme',
+      background: '#F0F4F8',
+      foreground: '#1F2937',
+      primary: '#2563EB',
+      'primary-focus': '#1D4ED8',
+      'primary-content': '#F0F4F8',
+      secondary: '#6B7280',
+      'secondary-focus': '#4B5563',
+      'secondary-content': '#F0F4F8',
+    },
+  ],
 };
+
+const myDesignSystem = daisyui.extend(DenaliCustomTheme);
+
+// Automatically install the design system in the app
+export function installDesignSystem(app) {
+  if (!app) {
+    app = createApp({});
+  }
+  app.use(install(myDesignSystem));
+  return app;
+}
