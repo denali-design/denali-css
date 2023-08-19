2fa8b61dcded4cf74959f5815d93992fd9754839
Trevor Greenleaf <trevor.greenleaf@yahooinc.com>
2023-08-19 15:33:58 -0700

    Removed Dasiy Ui Concept and clean up

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+        require("postcss-import"),
+        require("tailwindcss/nesting"),
+        require("tailwindcss")("./src/full/tailwind.config.js"),
+        require("autoprefixer"),
+    ],
+  }
